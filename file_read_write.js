63e574701348caf766ab1e47fd6b23f6398018fb
Bhargav007SGL <88825575+Bhargav007SGL@users.noreply.github.com>
2021-08-28 22:04:15 +0530

    File Uploaded externelly
    Upload my first file in nodejs

---
+++ file_read_write.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+// console.log(fs.readFileSync("./input.txt" , "utf8"));
+fs.writeFileSync("./input007.txt" , "I am god of thunder");
+fs.writeFile("./input007.txt" , "I am god of thunder 008" , {flag : "a"},(err , result)=>
+{
+    // console.log(result);
+});
+fs.writeFile("./input0078" , "Bhargacop" , (err,r)=>
+{
+    
+})
+
---
+++ utill.js
@@ -0,0 +1,19 @@
+// async function call()
+// {
+// const { readFile, writeFile } = require("fs");
+// var a = require("util");
+// var  x = a.promisify(readFile);
+// var y = a.promisify(writeFile);
+// // console.log();
+// var a =  await x("./input0078" , "utf8");
+// console.log(a);
+// // y("./input")
+// }
+// call();
+// console.log("Bhargv");
+
+// var a = "ai";
+// if(parseInt(a))
+// {
+//     console.log("op");
+// }
